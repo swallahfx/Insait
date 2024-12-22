9d446353c88068c51ab071d0fa0e88ddf401cf05
saheedEarly <saheed@getearlybird.ai>
2024-12-22 06:36:45 +0100

    INS-1: setting up the business logic

---
+++ app/controllers/question_controller.py
@@ -0,0 +1,18 @@
+from app import db
+from app.database.models import Question
+from app.integrations.openai import get_openai_answer
+
+
+class QuestionController:
+
+    @staticmethod
+    def ask_question(question_text):
+        # Get the answer from OpenAI
+        answer_text = get_openai_answer(question_text)
+
+        # Save the question and answer to the database
+        question = Question(question_text=question_text, answer_text=answer_text)
+        db.session.add(question)
+        db.session.commit()
+
+        return question, answer_text
